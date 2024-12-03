4eccc9d2d69ac5309d5e43ab6b7883d8dbcc9a56
Simon Wright <simon@pushface.org>
2024-12-03 22:02:06 +0000

    Convert reserved words to lower case.
    Actually, lowers case on all non-identifier and non-comment tokens;
    the non-identifier ones will be things like ':='.
    
      * src/ada_caser-processing.adb (Process): as summary.

--- src/ada_caser-processing.adb
+++ src/ada_caser-processing.adb
@@ -1,9 +1,12 @@
 --  SPDX-License-Identifier: Apache-2.0
 --  Copyright (C) 2024 Simon Wright <simon@pushface.org>
 
+with Ada.Strings;
 with Ada_Caser.Messages;
 with Ada_Caser.Dictionaries;
 with Libadalang.Common;
+with Ada.Strings.Wide_Wide_Fixed;
+with Ada.Strings.Wide_Wide_Maps.Wide_Wide_Constants;
 
 with Ada.Wide_Wide_Text_IO; use Ada.Wide_Wide_Text_IO;
 
@@ -15,14 +18,19 @@ package body Ada_Caser.Processing is
       Token : Common.Token_Reference := Analysis.First_Token (Unit);
       use type Common.Token_Reference;
       use type Common.Token_Kind;
+
+      use Ada.Strings.Wide_Wide_Fixed;
+      use Ada.Strings.Wide_Wide_Maps.Wide_Wide_Constants;
    begin
       Messages.Info ("processing " & Unit.Get_Filename);
       while Token /= Common.No_Token loop
          case Common.Kind (Common.Data (Token)) is
             when Common.Ada_Identifier =>
                Put (Dictionaries.Normalize (Common.Text (Token)));
-            when others =>
+            when Common.Ada_Comment =>
                Put (Common.Text (Token));
+            when others =>
+               Put (Translate (Common.Text (Token), Lower_Case_Map));
          end case;
          Token := Common.Next (Token);
       end loop;
