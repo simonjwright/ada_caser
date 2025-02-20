c9a57dc83add3dc02c644a1fd5ac64f6cbaf6716
Simon Wright <simon@pushface.org>
2025-02-20 12:48:04 +0000

    Reduce clutter in diagnostic reports.
    It's very easy for the Libadalang parser to get out of step, in which
    case you get a lot of "Skipped token" diagnostics.
    
    This change only outputs the first such diagnostic.
    
    This means that later real errors won't get reported until the first
    has been fixed. Error recovery would be difficult, given that
    Libadalang hasn't mamaged.
    
      * alire.toml (depends-on): add langkit_support.
      * src/ada_caser-main.adb (context): add Ada.Strings.Fixed,
          Langkit_Support.Diagnostics.
        (Main): when processing diagnostics, only output the first "Skipped
          token" message.

--- src/ada_caser-main.adb
+++ src/ada_caser-main.adb
@@ -6,7 +6,9 @@ with Ada_Caser.Messages;
 with Ada_Caser.Options;
 with Ada_Caser.Processing;
 with GNAT.Command_Line;
+with Ada.Strings.Fixed;
 with Libadalang.Analysis;
+with Langkit_Support.Diagnostics;
 
 procedure Ada_Caser.Main is
    use Libadalang;
@@ -33,9 +35,35 @@ begin
               Context.Get_From_File (Arg);
          begin
             if Unit.Has_Diagnostics then
-               for D of Unit.Diagnostics loop
-                  Messages.Error (Unit.Format_GNU_Diagnostic (D));
-               end loop;
+               Process_Diagnostics :
+               declare
+                  --  Once you get one skipped token, most of the
+                  --  tokens in the rest of the unit be skipped. This
+                  --  assumes that the first one will be the
+                  --  significant one.
+                  First_Skipped_Token : Boolean := True;
+               begin
+                  for D of Unit.Diagnostics loop
+                     Process_One_Diagnostic :
+                     declare
+                        Message : constant String :=
+                          Langkit_Support.Diagnostics.To_Pretty_String (D);
+                        use Ada.Strings.Fixed;
+                        Skipped_Token : constant Boolean :=
+                          Index (Message, "Skipped token") in Message'Range;
+                     begin
+                        if Skipped_Token then
+                           --  Only output the first "skipped token" message.
+                           if First_Skipped_Token then
+                              First_Skipped_Token := False;
+                              Messages.Error (Message);
+                           end if;
+                        else
+                           Messages.Error (Message);
+                        end if;
+                     end Process_One_Diagnostic;
+                  end loop;
+               end Process_Diagnostics;
                Messages.Error ("quitting because of errors", Quit => True);
             else
                Processing.Process (Unit);
@@ -47,7 +75,7 @@ begin
 exception
    when GNAT.Command_Line.Exit_From_Command_Line =>
       null; -- This is only raised after -h/--help, so nothing to do
-   when others                                   =>
+   when others =>
       if Messages.Number_Of_Errors = 0 then
          --  If non-sero, we've already output an Error message and set
          --  exit status to Failure.
